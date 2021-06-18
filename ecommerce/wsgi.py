4584112fbf078b8dc2fb48ae9ae54a34426e1fc7
Ravindra-Kanitkar <56260800+Ravindra-Kanitkar@users.noreply.github.com>
2021-06-18 18:05:20 +0530

    Final Commit 1

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
