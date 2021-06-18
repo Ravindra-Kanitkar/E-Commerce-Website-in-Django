00671be4dafe8975a951779988f22b7cca4077e4
Ravindra-Kanitkar <56260800+Ravindra-Kanitkar@users.noreply.github.com>
2021-06-18 15:54:18 +0530

    Profile changed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,9 +25,9 @@ TEMP_DIR = BASE_DIR / "templates"
 SECRET_KEY = "django-insecure-p+r28mh==e(rshl-ku@$7(fv!x9r_ja)i*(g_c1+g_zc(lc8ac"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://ravindra-ecommerce.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ravindra-ecommerce.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
