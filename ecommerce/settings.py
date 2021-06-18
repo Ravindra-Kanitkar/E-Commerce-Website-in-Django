121b9ad555e99c0d28c6e113a6d0e06ec2483073
Ravindra-Kanitkar <56260800+Ravindra-Kanitkar@users.noreply.github.com>
2021-06-18 18:15:56 +0530

    Final commit 3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -46,13 +46,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = "ecommerce.urls"
