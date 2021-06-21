49d4fb949ff01442bf6df2d6e0651e79bde59a52
Ravindra-Kanitkar <56260800+Ravindra-Kanitkar@users.noreply.github.com>
2021-06-21 10:33:59 +0530

    Done

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
     path('accounts/',include('accounts.urls'))
-] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
