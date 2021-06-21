fad04ef6e630ddd2bcde7c765b5b8398e2802de9
Ravindra-Kanitkar <56260800+Ravindra-Kanitkar@users.noreply.github.com>
2021-06-21 10:29:25 +0530

    Done

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,6 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
     path('accounts/',include('accounts.urls'))
-] +  static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
-urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
